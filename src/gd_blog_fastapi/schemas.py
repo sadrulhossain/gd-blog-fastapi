d446f6dbad4078f0035b3206a241f0fada83b6f8
S Sadrul Hossain <s.sadrul.hossain@portonics.com>
2026-08-13 22:55:49 +0600

    Refactor: sqlalchemy implementation.

--- src/gd_blog_fastapi/schemas.py
+++ src/gd_blog_fastapi/schemas.py
@@ -1,17 +1,32 @@
-import datetime
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
 
-from pydantic import BaseModel, ConfigDict, Field
+class UserBase(BaseModel):
+    name: str = Field(min_length=1, max_length=50)
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_length=120)
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    image_file: str | None
+    image_path: str
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
     content: str = Field(min_length=1)
-    author: str = Field(min_length=1, max_length=50)
 
 class PostCreate(PostBase):
-    pass
+    user_id: int
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
-    date_posted: str
+    user_id: int
+    author: UserResponse
+    date_posted: datetime
