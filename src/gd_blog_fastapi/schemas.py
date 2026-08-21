30a760d5bb903206608c8a6ef5fa73250189263c
S Sadrul Hossain <s.sadrul.hossain@portonics.com>
2026-08-21 21:26:25 +0600

    REfactor: file upload.

--- src/gd_blog_fastapi/schemas.py
+++ src/gd_blog_fastapi/schemas.py
@@ -14,7 +14,6 @@ class UserUpdate(BaseModel):
     name: str | None = Field(default=None, min_length=1, max_length=50)
     username: str | None = Field(default=None, min_length=1, max_length=50)
     email: EmailStr | None = Field(default=None, max_length=120)
-    image_file: str | None = Field(default=None, min_length=1, max_length=255)
 
 class Token(BaseModel):
     access_token: str
